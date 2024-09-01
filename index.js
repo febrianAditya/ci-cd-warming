4c5995de7bb9a83b148b9a93a04a6e35d012074f
FebrianAditya <febrian.aditya@student.upi.edu>
2024-09-02 02:20:42 +0700

    masuk

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Hello world v3" }');
+    res.send('{ "response": "Hello world Hahaha" }');
 });
 
 app.get('/will', function (req, res) {
